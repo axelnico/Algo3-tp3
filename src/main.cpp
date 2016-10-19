9d62dc6e035a7393e0ec1babe769d2733fdcffa7
Kevin Frachtenberg <kevinfra@users.noreply.github.com>
2016-10-19 01:35:03 -0300

    input ej1

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <utility>
 #include <vector>
+#include <tuple>
 #include <list>
 #include <string>
 #include <chrono>
@@ -41,5 +42,30 @@ int main(int argc, char *argv[]) {
     cout << "   -exp para experimentos" << endl;
     return -1;
   }
+
+  if (numeroDeEjercicio == 1) {
+    cout << "ingrese la cantidad de gimnasios, paradas y el tamaño de la mochila" << endl;
+    int n,m,k;
+    cin >> n >> m >> k;
+    cout << "ingrese en las siguientes " << n << " lineas xg, yg, pg, ubicacion y cantidad de posiones necesarias de los gym" << endl;
+    cout << "luego, en las siguientes " << m << " lineas xp, yp ubicacion de las paradas" << endl;
+    vector< tuple <int, int, int> > gimnasios(n);
+    for (int i = 0; i < n; i++) {
+      int x,y,p;
+      cin >> x >> y >> p;
+      tuple<int,int,int> gym = make_tuple(x,y,p);
+      gimnasios.push_back(gym);
+    }
+    vector< tuple <int, int> > paradas(m);
+    for (int i = 0; i < m; i++) {
+      int x,y;
+      cin >> x >> y;
+      tuple<int,int> pp = make_tuple(x,y);
+      paradas.push_back(pp);
+    }
+  }
+
+
+
   return 0;
 }
