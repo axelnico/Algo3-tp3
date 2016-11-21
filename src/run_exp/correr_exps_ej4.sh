c437ea4c45b0de2cc85721203e9ab33eb076a523
julianlen <julianlen@gmail.com>
2016-11-21 02:01:07 -0300

    franco esta re loco

--- src/run_exp/correr_exps_ej4.sh
+++ src/run_exp/correr_exps_ej4.sh
@@ -3,8 +3,5 @@ cd ..
 make clean
 make
 
-
-./tp3 4 -exp 40 < ../exp/Entradas/ej4/ej4_entrada_40_config1.txt > ../exp/Salidas/ej4/ej4_exp1.txt
-./tp3 4 -exp 40 < ../exp/Entradas/ej4/ej4_entrada_40_config2.txt >> ../exp/Salidas/ej4/ej4_exp1.txt
-./tp3 4 -exp 40 < ../exp/Entradas/ej4/ej4_entrada_40_config3.txt >> ../exp/Salidas/ej4/ej4_exp1.txt
-./tp3 4 -exp 40 < ../exp/Entradas/ej4/ej4_entrada_40_config4.txt >> ../exp/Salidas/ej4/ej4_exp1.txt
+./tp3 4 -expRandom 30 > ../exp/Entradas/ej4/ej4_entrada_40_config1.txt
+./tp3 4 -exp 120 < ../exp/Entradas/ej4/ej4_entrada_40_config1.txt > ../exp/Salidas/ej4/ej4_exp1.txt
